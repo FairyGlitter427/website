4f17a8c3553baa4491de60aae337da67d9b4b94c
Conny <Connykwong123@gmail.com>
2025-08-14 12:36:50 +0800

    updates
    -dynamic year
    -new concert photos
    -changed sentences

--- src/components/AppFooter/Footer.vue
+++ src/components/AppFooter/Footer.vue
@@ -2,14 +2,19 @@
     <footer>
         <div id="footer">
             <span>Email: <a href="mailto:Connykwong123@gmail.com">Connykwong123@gmail.com</a></span>
-            <span>© 2024 Conny Kwong</span>
+            <span>© {{ currentYear }} Conny Kwong</span>
         </div>
     </footer>
 </template>
 
 <script>
 export default {
-    name: 'Footer'
+    name: 'Footer',
+    data() {
+        return {
+            currentYear: new Date().getFullYear() // Dynamically get the current year
+        };
+    }
 }  
 </script>
 
